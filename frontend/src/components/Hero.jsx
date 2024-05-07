2c301c21ceac5d82eb910a6966855f8481ae7150
Thierry Billa <thierry.billa96@gmail.com>
2024-05-07 14:34:25 +0200

    hero banner code et sass et finition navbar sass et supression du bouton sign up

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navbar from './Navbar'; // Assure-toi que le chemin est correct
+import Navbar from './Navbar'; 
+import { FaSearch } from "react-icons/fa";
 import '../sass/hero.sass';
 
 function Hero() {
@@ -9,7 +10,10 @@ function Hero() {
       <h1 className="hero-title">Ready to Travel?</h1>
       <div className="search-bar">
         <input type="text" placeholder="Your next destination..." />
-        <button type="submit">Search</button>
+        <button type="submit"><span className="search-icon"><FaSearch /></span></button>
+      </div>
+      <div className="scroll-indicator">
+      <div className="mouse"></div>
       </div>
     </div>
   );
