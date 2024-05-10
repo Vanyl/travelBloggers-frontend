4243343e3e4b62c116b2a086633398c4717241fa
Yavanna Perez <yavannaperez@hotmail.com>
2024-05-10 17:05:38 +0200

    posts recent css

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,19 +1,49 @@
+import React, { useState } from 'react';
 import heroBanner2 from '../assets/images/hero-banner2.jpg';
-import '../sass/posts.sass'
+import '../sass/posts.sass';
 
 const Posts = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    // Create an array of data for your posts (you can replace this with actual data)
+    const postsData = [
+        { id: 1, title: 'Post 1', date: '2024-05-08', user: 'User 1', tag: 'Country A' },
+        { id: 2, title: 'Post 2', date: '2024-05-09', user: 'User 2', tag: 'Country B' },
+        { id: 3, title: 'Post 3', date: '2024-05-10', user: 'User 3', tag: 'Country C' },
+        { id: 4, title: 'Post 4', date: '2024-05-08', user: 'User 1', tag: 'Country A' },
+        { id: 5, title: 'Post 5', date: '2024-05-09', user: 'User 2', tag: 'Country B' },
+        { id: 6, title: 'Post 6', date: '2024-05-10', user: 'User 3', tag: 'Country C' }
+    ];
+
     return (
-        <div className="post-container">
-            <img className="post-img" src={heroBanner2} alt="post" />
-            <div className='info-container'>
-                <div className='avatar'>
-                    <img className='avatar-img' alt="avatar" />
+        <div className="posts-container">
+            {postsData.map((post) => (
+                <div
+                    key={post.id}
+                    className='post-container'
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
+                    <div className="image-container">
+                        <div className="img-overlay">
+                        <img className="post-img" src={heroBanner2} alt="post" />
+                        </div>
+                        {isHovered && (
+                            <div className='hover-info'>
+                                <p className='title'>{post.title}</p>
+                                <p className='date'>{post.date}</p>
+                            </div>
+                        )}
+                    </div>
+                    <div className='info-container'>
+                        <div className='avatar'>
+                            <img className='avatar-img' alt="avatar" />
+                        </div>
+                        <p className='user'>{post.user}</p>
+                        <p className='tag'>{post.tag}</p>
+                    </div>
                 </div>
-                <p className='user'>user</p> 
-                <p className='date'>date</p>
-            </div>
-            <p className='title'>titre</p>
-            <p className='tag'>tag country</p>
+            ))}
         </div>
     );
 };
