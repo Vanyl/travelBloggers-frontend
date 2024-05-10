7e6ce7a46c6dc1231b2ab2d2d746f5fe590f000e
Yavanna Perez <yavannaperez@hotmail.com>
2024-05-10 18:01:51 +0200

    authentification form simplier

--- frontend/src/components/Authentication.jsx
+++ frontend/src/components/Authentication.jsx
@@ -4,70 +4,39 @@ import { useState } from 'react'
 const Authentication = () => {
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [isActive, setIsActive] = useState(false);
+    const [isSignUp, setIsSignUp] = useState(false);
 
     const toggleLogin = () => {
-        setIsLoggedIn(!isLoggedIn);
+        setIsSignUp(!isSignUp);
     }
 
-    const activateContainer = () => {
-        setIsActive(true);
+    const AuthenticationForm = () => {
+        if (isSignUp) {
+            return (
+                <form className="register-form">
+                    <input type="text" name="email" placeholder="Email" />
+                    <input type="text" name="username" placeholder="Username" />
+                    <input type="password" name="password" placeholder="Password" />
+                    <button>create</button>
+                    <p className="message">Already registered? <a onClick={toggleLogin} href="#">Sign In</a></p>
+                </form>)
+        } else {
+            return (
+                <form className="login-form">
+                    <input type="text" name="email" placeholder="Email" />
+                    <input type="password" name="password" placeholder="Password" />
+                    <button>login</button>
+                    <p className="message">Not registered? <a onClick={toggleLogin} href="#">Create an account</a></p>
+                </form>)
+        }
     }
 
     return (
-    <div className={`authentication-container ${isLoggedIn ? 'login' : ''} ${isActive ? 'active' : ''}`}>
-    <div className="container-forms">
-        <div className="container-info">
-        <div className="info-item">
-            <div className="table">
-            <div className="table-cell">
-                <p>
-                Have an account?
-                </p>
-                <button className="btn" onClick={toggleLogin}>
-                Log in
-                </button>
-            </div>
+        <div className="login-page ">
+            <div className="form authentication-container">
+                <AuthenticationForm />
             </div>
         </div>
-        <div className="info-item">
-            <div className="table">
-            <div className="table-cell">
-                <p>
-                Don't have an account? 
-                </p>
-                <button className="btn" onClick={toggleLogin}>
-                Sign up
-                </button>
-            </div>
-            </div>
-        </div>
-        </div>
-
-        <div className="container-form">
-        <form className="form-item login">
-            <div className="table">
-            <div className="table-cell">
-                <input name="email" placeholder="Email" type="text" /><input name="password" placeholder="Password" type="Password" />
-                <button className="btn" onClick={activateContainer}>
-                Log in
-                </button>
-            </div>
-            </div>
-        </form>
-        <form className="form-item register">
-            <div className="table">
-            <div className="table-cell">
-                <input name="email" placeholder="Email" type="text" /><input name="Username" placeholder="Username" type="text" /><input name="password" placeholder="Password" type="Password" />
-                <button className="btn" onClick={activateContainer}>
-                Sign up
-                </button>
-            </div>
-            </div>
-        </form>
-        </div>
-    </div>
-    </div>
     )
 }
 
