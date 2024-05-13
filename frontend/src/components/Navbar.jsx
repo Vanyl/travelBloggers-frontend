c9230df976fefe29699543c7d86dcdc3b07a735a
Thierry Billa <thierry.billa96@gmail.com>
2024-05-13 16:18:54 +0200

    Navbar updated when logged in

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,23 +3,31 @@ import { FaSearch } from "react-icons/fa";
 import '../sass/navbar.sass';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
-
+function Navbar({ isLoggedIn }) {
+  
   return (
     <nav className="navbar">
-        <Link to="/" className="navbar-logo">Travel Bloggers</Link>   
-        <div className="navbar-links">
-          <Link to="/authentication" className="link" onClick={() => console.log('Login Soon')}>Login</Link>
-          <Link to="/authentication" className="link" onClick={() => console.log('Sign Up Soon')}>Sign Up</Link>
-          <Link to="/about" className="link" onClick={() => console.log('About Soon')}>About</Link>
-          <Link to="#" className="link" onClick={() => console.log('Contact Soon')}>Contact</Link>
-          <Link to="#" className="link" onClick={() => console.log('Search Soon')}><FaSearch /></Link>
+      <Link to="/" className="navbar-logo">Travel Bloggers</Link>   
+      <div className="navbar-links">
+        <Link to="/about" className="link">About</Link>
+        <Link to="#" className="link">Contact</Link>
+        <div className="right-links">
+          {isLoggedIn ? (
+            <>
+              <Link to="/my-account" className="link">My Account</Link>
+              <Link to="/logout" className="link">Logout</Link>
+            </>
+          ) : (
+            <>
+              <Link to="/authentication" className="link" onClick={() => console.log('Login Soon')}>Login</Link>
+              <Link to="/authentication" className="link" onClick={() => console.log('Sign Up Soon')}>Sign Up</Link>
+            </>
+          )}
+          <FaSearch className="link search-icon" />
         </div>
+      </div>
     </nav>
   );
 }
 
 export default Navbar;
-
-
-
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar'
 import Home from "../pages/Home"
 import About from "../pages/About"
@@ -8,13 +9,14 @@ import NoPage from "../pages/NoPage";
 
 
 function AppRouter() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
       <BrowserRouter>
-          <Navbar/>
+          <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/authentication" element={<Authentication />} />
+          <Route path="/authentication" element={<Authentication setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />} />
           <Route path="about" element = {<About/>} />
           <Route path="*" element={<NoPage />} />
         </Routes>
