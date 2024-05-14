917cdb3d47ac40ec0d0871746c5b27aed648b4c8
Yavanna Perez <yavannaperez@hotmail.com>
2024-05-14 16:00:09 +0200

    authentication sass fixed + contact page sass done

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,34 @@
+import '../sass/contact.sass'
+
 const Contact = () => {
-    return <div>This is the Contact page</div>;
+
+    return (
+      <>
+        <div className='contact-hero'>
+        <h2>Question ? <br/> Leave us a message</h2>
+        </div>
+          <div className='contact-container'>
+            <h3 className='contact-form-title'><span>Contact Form</span></h3>
+            <form className="contact-form" id="contact-form">
+              <input type="text" name="email" placeholder="Email" required/>
+              <input type="text" name="name" placeholder="Username" required/>
+              <input type="text" name="subject" placeholder="Subject" required/>
+              <textarea
+                id="contact-message" 
+                name="contact-message" 
+                placeholder="Your message" 
+                required
+                maxLength="250"
+                form="contact-form"
+                // value={message}
+                // onChange={(e) => setComment(e.target.value)}
+                >
+                </textarea>
+              <button type='submit'>contact</button>
+            </form>
+          </div>
+      </>
+    )
   };
   
   export default Contact;
