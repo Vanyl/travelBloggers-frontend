3fb9875f2c6b71fb6ba58acb9ed83215862baac3
Yavanna Perez <yavannaperez@hotmail.com>
2024-05-22 12:07:50 +0200

    fixed merge

--- frontend/src/components/ProfileSettings.jsx
+++ frontend/src/components/ProfileSettings.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
 import '../sass/profile-settings.sass';
 import { FaCog } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
